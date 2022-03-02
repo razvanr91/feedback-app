9afb29b158a1d4fb09854a6025bbd572921cfe4c
razvanr91 <razvan.roman91@outlook.com>
2022-03-02 15:30:19 +0000

    Create feedback list and data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from "react";
-import FeedbackItem from "./components/FeedbackItem";
+import React, { useState } from "react";
+import FeedbackList from "./components/FeedbackList";
 import Header from "./components/Header";
+import FeedbackData from "./data/FeedbackData";
 
 const App = () => {
+    const [feedback, setFeedback] = useState(FeedbackData);
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackItem />
+                <FeedbackList feedback={feedback} />
             </div>
         </>
     )
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, inventore."
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo animi officiis illum velit quae esse nobis quasi deleniti asperiores tempore!"
+    },
+    {
+        id: 3,
+        rating: 7,
+        text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo, sunt accusantium, hic quae voluptatum cupiditate provident voluptatem commodi nemo quaerat, laboriosam sed tenetur sapiente modi. Quae quibusdam dolorem iste deleniti."
+    }
+];
+
+export default FeedbackData;
