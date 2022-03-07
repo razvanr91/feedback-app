2d6fb97b98379269b88aca639efba9d745f688b9
razvanr91 <razvan.roman91@outlook.com>
2022-03-07 13:29:10 +0000

    Create edit and delete for backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -38,8 +38,9 @@ export const FeedBackProvider = ({ children }) => {
     }
 
     // Delete feedback
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
         if (window.confirm("Are you sure you want to delete?")) {
+            await fetch(`/feedback/${id}`, { method: "DELETE" });
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
@@ -52,8 +53,18 @@ export const FeedBackProvider = ({ children }) => {
         });
     }
 
-    const updateFeedback = (id, updatedItem) => {
-        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updatedItem } : item));
+    const updateFeedback = async (id, updatedItem) => {
+        const response = await fetch(`/feedback/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedItem)
+        });
+
+        const data = await response.json();
+
+        setFeedback(feedback.map((item) => item.id === id ? { ...item, ...data } : item));
     }
 
     return (
